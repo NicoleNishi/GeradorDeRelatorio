adc5134354e54bc510f45ba0bcd869194dfda55f
NicoleNishi <nicole.nishi04@gmail.com>
2025-07-04 16:18:14 -0300

    Adição de critério de odenação por preço

---
+++ ComparatorPreco.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class ComparatorPreco implements Comparator<Produto> {
+    @Override
+    public int compare(Produto prod1, Produto prod2) {
+        return Double.compare(prod1.getPreco(), prod2.getPreco());
+    }
+}
