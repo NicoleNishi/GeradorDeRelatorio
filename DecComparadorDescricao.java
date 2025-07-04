3a916bd20671e0d2980c29bec381488dbbc8cabe
NicoleNishi <nicole.nishi04@gmail.com>
2025-07-04 16:46:44 -0300

    Adição do critério de odenação por descrição decrescente

---
+++ DecComparadorDescricao.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class DecComparadorDescricao implements Comparator<Produto> {
+    @Override
+    public int compare(Produto prod1, Produto prod2) {
+        return prod2.getDescricao().compareTo(prod1.getDescricao());
+    }
+}
