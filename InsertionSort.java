8c0ce66626d8a92565d3348571b8717ccc503b07
NicoleNishi <nicole.nishi04@gmail.com>
2025-07-04 16:50:38 -0300

    Adição do algoritmo de ordenação Insertion Sort

---
+++ InsertionSort.java
@@ -0,0 +1,17 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class InsertionSort implements AlgoritmoOrd{
+    @Override
+    public void ordenar(List<Produto> produtos, Comparator<Produto> criterio) {
+        for (int i = 1; i < produtos.size(); i++) {
+            Produto chave = produtos.get(i);
+            int j = i - 1;
+            while (j >= 0 && criterio.compare(produtos.get(j), chave) > 0) {
+                produtos.set(j + 1, produtos.get(j));
+                j--;
+            }
+            produtos.set(j + 1, chave);
+        }
+    }
+}
